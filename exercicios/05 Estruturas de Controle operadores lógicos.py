a948e04ba0fcad5a9a41b26ffc7ba414581b98d6
AMFDZN <amfdzn@gmail.com>
2026-08-04 21:24:34 -0300

    fim de aula terça

--- exercicios/05 Estruturas de Controle operadores lógicos.py
+++ exercicios/05 Estruturas de Controle operadores lógicos.py
@@ -91,11 +91,13 @@ if idade>0:
         print("Você não pode dirigir")
 else:
     print("Idade inválida")
+    
+    
   
 # Exercício 5  
 # Pergunte ao usuário se ele tem dinheiro s ou n e convite s ou n. 
 # Informe se ele pode entrar no evento, ou porque tem dinheiro ou tem convite. 
- 
+print("exercício 6")
 # Exercício 6  
 # Pergunte ao usuário se está chovendo s ou n. 
 # Informe se ele pode sair sem guarda-chuva, ou se deve levar. 
