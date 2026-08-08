f8a135ba5341ebe8c19b83f7f3844de4f05ada50
AMFDZN <amfdzn@gmail.com>
2026-08-07 21:22:06 -0300

    exercícios e aulas

--- excel/Manipulacao_Excel_Openpyxl_01.py
+++ excel/Manipulacao_Excel_Openpyxl_01.py
@@ -1,3 +1,5 @@
+#pip install openpyxl
+
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 from openpyxl.styles import Font, PatternFill, Border, Side, Alignment
--- excel/automacao_arquivos_excel.py_03.py
+++ excel/automacao_arquivos_excel.py_03.py
@@ -1,5 +1,3 @@
-#pip install pandas
-
 import pandas as pd
 import numpy as np
 import os
